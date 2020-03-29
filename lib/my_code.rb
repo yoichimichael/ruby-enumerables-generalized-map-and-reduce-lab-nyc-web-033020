ef59be7fce72296a96453b726db78c2175d5200d
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-29 06:20:43 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,14 +9,21 @@ def map(array)
   new_array
 end
 
-def reduce(array, starting_point = 0)
-  total = starting_point
-  i = 0
-  while i < array.length do
-    if array[i].class = Integer
-      total += yield(0 , array[i])
-    elsif 
-    i += 1
-  end
-  total
+
+#the video walkthrough link was unavailable, but I was able to find this solution online
+#is there a way to solve this without the splat operator?
+def reduce(array, *starting)
+    if starting[0]
+      value = starting[0]
+      count = 0
+    else
+      value = array[0]
+      count = 1
+    end
+
+    while count < array.length do
+      value =  yield(value,array[count])
+      count+=1
+    end
+    value
 end
