0c7d32c8758a2f606468214d10548c5cc7905f28
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-29 01:46:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(array)
+  new_array = []
+  i = 0
+  while i < array.length do
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
+
+def reduce(array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while i < array.length do
+    if array[i].class = Integer
+      total += yield(0 , array[i])
+    elsif 
+    i += 1
+  end
+  total
+end
